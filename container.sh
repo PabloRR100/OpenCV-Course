3e911f3bb8bcb17b498f3700754030b7a4324747
PabloRR100 <pabloruizruiz10@gmail.com>
2019-09-01 18:31:18 +0200

    updating bash file to connect to container

--- container.sh
+++ container.sh
@@ -3,4 +3,6 @@ open -a XQuartz
 defaults write org.macosforge.xquartz.X11 enable_iglx -bool true
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $IP
-docker run -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD/:/home/jovyan/work/ -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 -it vishwesh5/opencv:4-1-0-conda /bin/bash
+WORKDIR=/home/jovyan/work/
+# docker run -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD/:/home/jovyan/work/ -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 -it vishwesh5/opencv:4-1-0-conda /bin/bash
+docker run -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD/:$WORKDIR -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 -it opencv /bin/bash
